d11e9efdb587e05afe72d9e71eca881de535cb3c
vrmaxin <vrmaxin@yeah.net>
2019-04-29 19:08:33 +0800

    koa的https请求

--- 22.js
+++ 22.js
@@ -1,6 +1,9 @@
 const Koa = require('koa')
 const path = require('path')
 const static = require('koa-static')
+const sslify = require('koa-sslify').default; //http强制HTTPS
+const https = require('https');//node内置https server
+const fs = require('fs');
 const app = new Koa()
 
 const staticPath = './static'
@@ -13,8 +16,19 @@ app.use(async (ctx) => {
   ctx.body = 'hello worldasd'
 })
 
-app.listen(3000, () => {
-  console.log(__dirname)
-  console.log(path.join(__dirname, staticPath))
-  console.log('server is starting at port 3000')
-})
+app.use(sslify())
+
+// app.listen(3000, () => {
+//   console.log(__dirname)
+//   console.log(path.join(__dirname, staticPath))
+//   console.log('server is starting at port 3000')
+// })
+
+var options = {
+  key: fs.readFileSync('./private_key.pem'), //私钥文件路径
+  cert: fs.readFileSync('./ca-cert.pem') //证书文件路径
+};
+
+https.createServer(options, app.callback()).listen(9527, () => {
+  console.log(`server running success at 9527`)
+});
